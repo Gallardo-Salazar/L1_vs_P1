55a71a78afc716e64f61bbaa3a93446049a81af7
unknown <gallardoirg@gmail.com>
2022-01-07 16:58:05 -0600

    Initial Commit

---
+++ pointclouds/L1/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 42041620,
+    "boundingBox": {
+        "lx": 489369.61079045719,
+        "ly": 2135852.659622119,
+        "lz": 2222.8806827124513,
+        "ux": 489799.2206633767,
+        "uy": 2136282.2694950385,
+        "uz": 2652.4905556319586
+    },
+    "tightBoundingBox": {
+        "lx": 489369.6107633288,
+        "ly": 2135852.6596585789,
+        "lz": 2222.8806665106754,
+        "ux": 489685.2533633288,
+        "uy": 2136282.2694585786,
+        "uz": 2304.9944665106755
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.7205307483673097,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ pointclouds/P1/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 13719509,
+    "boundingBox": {
+        "lx": 489368.34750976567,
+        "ly": 2135898.6844018555,
+        "lz": 2220.331077194214,
+        "ux": 489692.6148780823,
+        "uy": 2136222.951770172,
+        "uz": 2544.598445510864
+    },
+    "tightBoundingBox": {
+        "lx": 489368.3475,
+        "ly": 2135898.6844,
+        "lz": 2220.3311,
+        "ux": 489640.3483,
+        "uy": 2136222.9518,
+        "uz": 2263.8093
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.8082377910614015,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
